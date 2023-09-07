ee3a43bef7943887eb0f42e584a51df7e57723a4
Ruslan Feduleyev <8reflections@gmail.com>
2023-09-08 03:35:25 +0600

    New features

--- components/intro/intro.js
+++ components/intro/intro.js
@@ -1,14 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
   const parallaxElements = document.querySelectorAll('[data-parallax-mouse]');
-  parallaxElements.forEach(item => {
-    const parallaxSize = Number(item.getAttribute('data-parallax-size'))
-    const ratio = 5
-    const resultTransform = parallaxSize - ratio
-    window.addEventListener('mousemove', function (e) {
-      let x = e.clientX / window.innerWidth;
-      let y = e.clientY / window.innerHeight;
-      item.style.transform = 'translateX(-' + x * resultTransform + 'px)';
-    });
-  })
+
+
+  const handleParallax = () => {
+    parallaxElements.forEach(item => {
+      const parallaxSize = Number(item.getAttribute('data-parallax-size'))
+      const ratio = 5
+      const resultTransform = parallaxSize - ratio
+
+      if (window.innerWidth >= 1200) {
+        window.addEventListener('mousemove', function (e) {
+          let x = e.clientX / window.innerWidth;
+          item.style.transform = 'translateX(-' + x * resultTransform + 'px)';
+        });
+      } else {
+        window.addEventListener('scroll', function (e) {
+          const direction = item.getAttribute('data-parallax-direction')
+          const scrollY = window.scrollY
+          if (direction) {
+            if (direction === 'left') {
+              item.style.transform = 'translateX(' + scrollY / 1500 * resultTransform + 'px)';
+            } else if (direction === 'right') {
+              item.style.transform = 'translateX(-' + scrollY / 1500 * resultTransform + 'px)';
+            }
+          } else {
+            item.style.transform = 'translateX(-' + scrollY / 300 * resultTransform + 'px)';
+          }
+
+
+        });
+      }
+    })
+
+  }
+
+  handleParallax()
+  window.addEventListener('resize', handleParallax)
 
 })
